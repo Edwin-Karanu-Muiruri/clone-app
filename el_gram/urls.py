2eaf81ea0bff98369f4a480aff7c732e4d792444
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-05-31 20:22:59 +0300

    Installed the django-registration module. Added it to the urls.

--- el_gram/urls.py
+++ el_gram/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gram.urls')),
+    path('accounts/',include('django_registration.backends.one_step.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
 ]
