cc88d7a9b7c0053defb9d9f7816c20ac13dc0f21
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-06-01 11:45:28 +0300

    Created the sign up page. Also created the login page(error while loading it). The django_registration folder in the templates is to handle all the registration routes.

--- el_gram/settings.py
+++ el_gram/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gram',
+    'django_registration',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+ACCOUNT_ACTIVATION_DAYS = 14
