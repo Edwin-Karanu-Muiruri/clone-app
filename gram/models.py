5ffaced6dafa6b63bf0eb499a6bab505b8385f3f
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-06-01 16:49:33 +0300

    Created the first models and made the migrations.

--- gram/models.py
+++ gram/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image_name = models.CharField(max_length = 50)
+    image_caption = models.CharField(max_length = 50)
+    comments = models.CharField(max_length = 50)
 
-
+class Profile(models.Model):
+    bio = models.CharField(max_length = 50)
