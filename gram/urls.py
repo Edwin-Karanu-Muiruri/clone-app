3a7d9e17151cb88989304bf057f7f2ad81635932
Edwin Karanu Muiruri <edwin.karanu.muiruri@gmail.com>
2020-06-01 16:46:28 +0300

    Created the database and connected it to the application.

--- gram/urls.py
+++ gram/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('',views.welcome,),
     path('home/',views.home,),
+    path('profile/',views.profile,),
 ]
--- gram/views.py
+++ gram/views.py
@@ -5,3 +5,5 @@ def welcome(request):
     return render(request,'welcome.html')
 def home(request):
     return render(request,'home.html')
+def profile(request):
+    return render(request,'profile.html')
